e1f45f08fa4d3789272f523abf051fa2eed1edfb
wodus77 <ghkdwo77@gmail.com>
2018-06-27 14:12:41 +0900

    fix write JSON for subgenre

--- src/test/java/com/kthcorp/cmts/service/CcubeServiceTest.java
+++ src/test/java/com/kthcorp/cmts/service/CcubeServiceTest.java
@@ -110,7 +110,7 @@ public class CcubeServiceTest {
 	@Test
 	public void test_getJsonArrayForCcubeOutput() throws Exception {
 		Map<String, Object> ins = new HashMap();
-		ins.put("idx",21825);
+		ins.put("idx",22);
 		JsonArray contents = ccubeService.getJsonArrayForCcubeOutput(null, "CcubeContent", ins);
 		System.out.println("#RES:"+contents.toString());
 	}
