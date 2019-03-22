082f3e9f1c39da4de3119046e33a7525407f5d9a
Jae12 <hanaistory@gmail.com>
2019-03-22 16:19:02 +0900

    add new branch cmts_jae12_190322

--- src/test/java/com/kthcorp/cmts/util/AES256UtilTest.java
+++ src/test/java/com/kthcorp/cmts/util/AES256UtilTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.net.URLEncoder;
 
 import static org.junit.Assert.*;
-
+ 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class AES256UtilTest {
